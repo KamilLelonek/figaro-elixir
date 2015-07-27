1215457cae3c89164720477691ea01f5fab02214
Kamil Lelonek <squixy.sln@gmail.com>
2015-07-27 20:59:12 +0200

    Updating environmental variables

--- lib/figaro_elixir/env.ex
+++ lib/figaro_elixir/env.ex
@@ -1,4 +1,22 @@
 defmodule FigaroElixir.Env do
+  def update_env(config) do
+    config
+      |> upcase_keys
+      |> put_or_delete_env
+  end
+
+  def put_or_delete_env(map) do
+    Enum.each(
+      map,
+      fn { key, value } ->
+        put_or_delete_env(key, value)
+      end
+    )
+  end
+
+  def put_or_delete_env(key, "~"), do: System.delete_env(key)
+  def put_or_delete_env(key, val), do: System.put_env(key, val)
+
   def upcase_keys(map) do
     map
       |> map_all_key_to_uppercase
--- test/figaro_elixir/env_test.exs
+++ test/figaro_elixir/env_test.exs
@@ -6,4 +6,24 @@ defmodule FigaroElixir.EnvTest do
   test "formatting keys to uppercase" do
     assert Env.upcase_keys(%{ "a" => "a" }) == %{ "A" => "a" }
   end
+
+  test "updating system environmental variables" do
+    config = %{"a" => "1"}
+
+    Env.update_env(config)
+
+    assert System.get_env("A") == "1"
+  end
+
+  test "nullify system variables" do
+    config = %{"b" => "~"}
+
+    System.put_env("B", "B")
+
+    assert System.get_env("B") == "B"
+
+    Env.update_env(config)
+
+    assert System.get_env("B") == nil
+  end
 end
