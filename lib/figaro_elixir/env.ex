e8280d7c8bae66ce803bf0a9fafd7aabf52c1179
Kamil Lelonek <squixy.sln@gmail.com>
2015-07-28 14:25:15 +0200

    Making values as strings

--- lib/figaro_elixir/env.ex
+++ lib/figaro_elixir/env.ex
@@ -23,7 +23,7 @@ defmodule FigaroElixir.Env do
       |> convert_array_to_map
   end
 
-  defp upcase_keys_transformation({key, value}), do: { String.upcase(key), value }
+  defp upcase_keys_transformation({key, value}), do: { String.upcase(key), to_string(value) }
   defp map_all_key_to_uppercase(map),            do: map   |> Enum.map(&upcase_keys_transformation/1)
   defp convert_array_to_map(array),              do: array |> Enum.into(%{})
 end
