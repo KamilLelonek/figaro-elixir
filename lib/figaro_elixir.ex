9bc9c8709da4f3a5482286ad36d829d38f4127e4
Kamil Lelonek <squixy.sln@gmail.com>
2015-07-28 15:36:53 +0200

    Testing FigaroElixir application

--- lib/figaro_elixir.ex
+++ lib/figaro_elixir.ex
@@ -1,10 +1,22 @@
 defmodule FigaroElixir do
-  def start(_type, _args) do
-    import Supervisor.Spec
+  use Application
 
-    children = []
-    options  = [strategy: :one_for_one, name: Figaro.Supervisor]
+  alias FigaroElixir.Yaml
+  alias FigaroElixir.Config
+  alias FigaroElixir.Env
 
-    Supervisor.start_link(children, options)
+  @agent_name :config
+
+  defp children,            do: [Supervisor.Spec.worker(FigaroElixir, [])]
+  defp options,             do: [strategy: :one_for_one, name: FigaroElixir.Supervisor]
+  def  start(_, _),         do: Supervisor.start_link(children, options)
+
+  def  env,                 do: Agent.get(@agent_name, &(&1))
+  defp start_agent(config), do: Agent.start_link(fn -> config end, name: @agent_name)
+
+  def start_link do
+    config = Yaml.load_file |> Config.extract
+    Env.update_env(config)
+    start_agent(config)
   end
 end
--- test/figaro_elixir_test.exs
+++ test/figaro_elixir_test.exs
@@ -1,4 +1,34 @@
 defmodule FigaroElixirTest do
   use ExUnit.Case
 
+  setup do
+    System.put_env("B", "B")
+    System.put_env("D", "D")
+    Application.start(:figaro_elixir)
+
+    on_exit fn ->
+      Application.stop(:figaro_elixir)
+      System.delete_env("A")
+      System.delete_env("C")
+    end
+  end
+
+  test "parsing config.yml file" do
+    assert FigaroElixir.env["a"] == "a"
+    assert FigaroElixir.env["c"] == "1"
+  end
+
+  test "loading into system environment" do
+    assert System.get_env("A") == "a"
+    assert System.get_env("C") == "1"
+  end
+
+  test "loading into system environment nullifies environment-specifc overrides" do
+    assert System.get_env("B") == nil
+    assert System.get_env("D") == nil
+  end
+
+  test "raises an error if application.yml isn't found" do
+    assert { :error, _reason } = Application.start(:figaro)
+  end
 end
