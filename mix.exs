d3442b51c2ce831dec21f4c1abb613d4bdb9363f
Kamil Lelonek <squixy.sln@gmail.com>
2015-07-28 21:40:01 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FigaroElixir.Mixfile do
   def project do
     [
       app:             :figaro_elixir,
-      version:         "0.0.1",
+      version:         "1.0.0",
       elixir:          "~> 1.0",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
